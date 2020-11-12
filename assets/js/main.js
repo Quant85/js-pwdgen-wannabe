514cd86f7192b62a0a2bccfe907b2df802d9a4e7
Quant85 <antonio.85.q@gmail.com>
2020-11-12 15:47:35 +0100

    Implementazione button per attivare funzione di generazione password

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,29 @@
-var yourName = prompt("Ciao, come ti chiami?");
 
-var yourSurname = prompt("Piacere " + yourName +", di cognome?");
+// var yourName, yourSurname, yourAge, yourFavouriteColor, yourFavouriteNumber, yourPassword;
 
-var yourAge = prompt( yourName + " " + yourSurname + ", quanti anni hai?");
-
-var yourFavouriteColor = prompt( yourName + ", complimenti! " + yourAge + " sono anni ruggenti!" +  " E qual'è il tuo colore preferito?");
-
-var yourFavouriteNumber = prompt( yourName + ", un'ultima domanda prima di scoprire la tua nuova fantastica password. Qual'è il tuo numero preferito?");
-    confirm( yourName + " sei pronto a scoprire la tua password SupeInsiCula?");
-
+var yourName;
+var yourSurname;
+var yourAge;
+var yourFavouriteColor;
+var yourFavouriteNumber;
 var yourPassword;
+var questPasswordGeneretor;
 
-yourPassword = yourName + yourSurname + yourFavouriteColor + yourFavouriteNumber + (19) + yourAge;
+questPasswordGeneretor = function() {
 
-password.innerHTML = yourPassword;
+    yourName = prompt("Ciao, come ti chiami?");
+    
+    yourSurname = prompt("Piacere " + yourName +", di cognome?");
+    
+    yourAge = prompt( yourName + " " + yourSurname + ", quanti anni hai?");
+    
+    yourFavouriteColor = prompt( yourName + ", complimenti! " + yourAge + " sono anni ruggenti!" +  " E qual'è il tuo colore preferito?");
+    
+    yourFavouriteNumber = prompt( yourName + ", un'ultima domanda prima di scoprire la tua nuova fantastica password. Qual'è il tuo numero preferito?");
+    
+        confirm( yourName + " sei pronto a scoprire la tua password SupeInsiCula?");
+    
+    yourPassword = yourName + yourSurname + yourFavouriteColor + yourFavouriteNumber + (19) + yourAge;
+    
+    password.innerHTML = yourPassword;
+};
